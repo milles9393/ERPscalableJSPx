00ea1da5d08c84d6b8e9b2d64fbe4aa1bc1756a4
Mattias <mattias@gitgud.eu>
2019-03-04 11:06:36 +0100

    test

--- src/java/WebClient/Models/DBConnectionManager.java
+++ src/java/WebClient/Models/DBConnectionManager.java
@@ -28,10 +28,7 @@ public class DBConnectionManager {
         }
     }
 
-    public void CreateIssue(){
-    }
-
-    public ArrayList<Issue> getIssueList(){ //
+    public ArrayList<Issue> getIssueList(){
         ResultSet rs = null;
         String sql = "SELECT issueId, skillId, status, comment FROM Issue";
         ArrayList<Issue> test = new ArrayList<>();
@@ -109,44 +106,14 @@ public class DBConnectionManager {
         return test;
     }
 
-    public void createNewIssue(int skill, String comment){
 
-        try{
-            PreparedStatement newIssueStmt = con.prepareStatement("insert into Issue (skillId, comment) values (?, ?)");
-
-            newIssueStmt.setInt(1, skill);
-            newIssueStmt.setString(2, comment);
-            newIssueStmt.executeUpdate();
-        }
-            catch(Exception e) {
-            System.out.println(e);
-            }
-    }
-
-    public void deleteIssue(int skillId){
-
-        try{
-            String deleteSqlQuery = "DELETE FROM Issue where (issueId) = (?)";
-            PreparedStatement deleteIssueStmt = con.prepareStatement(deleteSqlQuery);
-            deleteIssueStmt.setInt(1, skillId);
-            int k = deleteIssueStmt.executeUpdate();
-            System.out.println(k +" records deleted");
-        }
-
-        catch (Exception e){
-            System.out.println("error deleting!!!! --->: " + e);
-        }
-    }
-
-    public void updateIssue(int issueId, int skillId, String status, String comment) {
+    public void updateIssueStatus(int issueId, String status) {
 
         try {
-            String updateSqlQuery = "UPDATE Issue set status = ?, comment = ?, skillId = ? WHERE issueId = ?";
+            String updateSqlQuery = "UPDATE Issue set status = ? WHERE issueId = ?";
             PreparedStatement updateIssueStmt = con.prepareStatement(updateSqlQuery);
             updateIssueStmt.setString(1, status);
-            updateIssueStmt.setString(2, comment);
-            updateIssueStmt.setInt(3, skillId);
-            updateIssueStmt.setInt(4, issueId);
+            updateIssueStmt.setInt(2, issueId);
 
             updateIssueStmt.executeUpdate();
         } catch (Exception e) {
@@ -155,18 +122,14 @@ public class DBConnectionManager {
         }
     }
 
-    public void updateAssignment(int assignmentId, int employeeId, String name, int allottedTime, int passedTime, String comment){
+    public void updateAssignmentTime(int assignmentId, int passedTime){
 
         try {
 
-            String updateSqlQuery = "UPDATE Assignment SET employeeId = ?, name = ?, allottedTime = ?, passedTime = ?, comment = ? WHERE assignmentId = ?";
+            String updateSqlQuery = "UPDATE Assignment SET passedTime = ? WHERE assignmentId = ?";
             PreparedStatement updateIssueStmt = con.prepareStatement(updateSqlQuery);
-            updateIssueStmt.setInt(1, employeeId);
-            updateIssueStmt.setString(2, name);
-            updateIssueStmt.setInt(3, allottedTime);
-            updateIssueStmt.setInt(4, passedTime);
-            updateIssueStmt.setString(5, comment);
-            updateIssueStmt.setInt(6, assignmentId);
+            updateIssueStmt.setInt(1, passedTime);
+            updateIssueStmt.setInt(2, assignmentId);
 
             updateIssueStmt.executeUpdate();
 
@@ -174,26 +137,6 @@ public class DBConnectionManager {
             System.out.println("error updating!!!! --->: " + e);
         }
     }
-
-    public void createNewAssignment(int issueId, String name, int allottedTime, int passedTime, String comment){
-
-        try {
-
-            String createSqlQuery = "INSERT INTO Assignment (issueId, name, allottedTime, passedTime, comment) VALUES (?, ?, ? , ?, ?)";
-
-            PreparedStatement updateIssueStmt = con.prepareStatement(createSqlQuery);
-            updateIssueStmt.setInt(1, issueId);
-            updateIssueStmt.setString(2, name);
-            updateIssueStmt.setInt(3, allottedTime);
-            updateIssueStmt.setInt(4, passedTime);
-            updateIssueStmt.setString(5, comment);
-            updateIssueStmt.executeUpdate();
-
-        } catch (Exception e) {
-            System.out.println("error updating!!!! --->: " + e);
-        }
-    }
-
 
     public ArrayList<Employee> getEmployees() {
 
